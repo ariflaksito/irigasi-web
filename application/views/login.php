9e427a7c0c27510a12519c39cf0c9ba101095c51
Arif Laksito <arif.laksito@gmail.com>
2017-09-26 20:14:50 +0700

    Add placeholder input login

--- application/views/login.php
+++ application/views/login.php
@@ -43,11 +43,11 @@
 
     <form action="login/do_login" method="post">
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" name="usr">
+        <input type="text" class="form-control" name="usr" placeholder="Username">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" name="pwd">
+        <input type="password" class="form-control" name="pwd" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
